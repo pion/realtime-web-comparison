22c321addc0cfafb1228cfb5905b04584e99feac
Srayan Jana <valorzard.kiko@outlook.com>
2025-11-04 23:02:02 -0800

    Remove unneeded code

--- server-sent-events/axum-sse/src/main.rs
+++ server-sent-events/axum-sse/src/main.rs
@@ -1,29 +1,21 @@
 use async_stream::stream;
 use axum::{
     Router,
-    extract::State,
     response::sse::{Event, Sse},
     routing::get,
 };
 use std::convert::Infallible;
 
 use futures::stream::Stream;
-use tokio::sync::broadcast;
-use tokio::sync::broadcast::Sender;
 
 #[tokio::main]
 async fn main() {
-    let (tx, _rx) = broadcast::channel::<String>(100);
-    let app = Router::new().route("/", get(sse_handler)).with_state(tx);
+    let app = Router::new().route("/", get(sse_handler));
     let listener = tokio::net::TcpListener::bind("0.0.0.0:7999").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
-async fn sse_handler(
-    State(tx): State<Sender<String>>,
-) -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
-    // broadcast::Receiver
-    let mut rx = tx.subscribe();
+async fn sse_handler() -> Sse<impl Stream<Item = Result<Event, Infallible>>> {
     Sse::new(stream! {
          // Send message so we can test the server performance
             for i in (10..=510).step_by(10) {
